9a49653b6139e9c8f52744f5d342515615bea4cf
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2024-04-08 15:18:23 +0200

    add method get one task

--- records/task.record.ts
+++ records/task.record.ts
@@ -57,4 +57,15 @@ export class TaskRecord implements TaskEntity {
 
     return results.map((obj) => new TaskRecord(obj));
   }
+
+  static async getOne(id: string): Promise<TaskRecord | null> {
+    const [results] = (await pool.execute(
+      'SELECT * FROM `tasks` WHERE `id` = :id',
+      {
+        id,
+      }
+    )) as TaskRecordResults;
+
+    return results.length === 0 ? null : new TaskRecord(results[0]);
+  }
 }
--- routes/tasks.routes.ts
+++ routes/tasks.routes.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
 import { TaskRecord } from '../records/task.record';
+import { ValidationError } from '../utils/errors';
 
 export const tasksRouter = Router();
 
-tasksRouter.get('/', async (req, res) => {
-  const taskRecord = await TaskRecord.getAll();
-  res.json({ taskRecord });
-});
+tasksRouter
+  .get('/', async (req, res) => {
+    const taskRecord = await TaskRecord.getAll();
+    res.json({ taskRecord });
+  })
+  .get('/:id', async (req, res) => {
+    const taskId = req.params.id;
+    const task = await TaskRecord.getOne(taskId);
+
+    if (!task) {
+      throw new ValidationError('There is no such task');
+    }
+
+    res.json(task);
+  });
