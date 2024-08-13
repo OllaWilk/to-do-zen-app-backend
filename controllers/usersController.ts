dc884d99423177654fac94c4602e12b1559ad61b
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2024-08-13 18:56:18 +0200

    add controlers to userRoute and add cookie-parser and express-session

---
+++ controllers/usersController.ts
@@ -0,0 +1,47 @@
+import { Response, Request } from 'express';
+import { UserRecord } from '../records/user.record';
+import { createToken } from '../utils/tokenGenerator';
+import { ValidationError } from '../utils/errors';
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  const usersRecord = await UserRecord.getAll();
+  res.json({ usersRecord });
+};
+
+export const signupUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const exists = await UserRecord.getOneByEmail(email);
+
+    if (exists) {
+      throw new ValidationError(
+        'It looks like you already have an account with us. Please try logging in'
+      );
+    }
+
+    const user = new UserRecord({
+      ...req.body,
+      email,
+      password: password,
+    });
+
+    await user.signup();
+
+    const token = createToken(user.id, '3d');
+
+    res.status(201).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserRecord.login(email, password);
+    const token = createToken(user.id, '3d');
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -1,50 +1,14 @@
 import { Router } from 'express';
-import { ValidationError } from '../utils/errors';
-import { createToken } from '../utils/tokenGenerator';
-import { UserRecord } from '../records/user.record';
+import {
+  getAllUsers,
+  loginUser,
+  signupUser,
+} from '../controllers/usersController';
 
 export const usersRouter = Router();
 
 usersRouter
-  .get('/', async (req, res) => {
-    const usersRecord = await UserRecord.getAll();
-    res.json({ usersRecord });
-  })
+  .get('/', getAllUsers)
+  .post('/login', loginUser)
 
-  .post('/login', async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const user = await UserRecord.login(email, password);
-      const token = createToken(user.id, '3d');
-      res.status(200).json({ user, token });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  })
-
-  .post('/signup', async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const exists = await UserRecord.getOneByEmail(email);
-
-      if (exists) {
-        throw new ValidationError(
-          'It looks like you already have an account with us. Please try logging in'
-        );
-      }
-
-      const user = new UserRecord({
-        ...req.body,
-        email,
-        password: password,
-      });
-
-      await user.signup();
-
-      const token = createToken(user.id, '3d');
-
-      res.status(201).json({ user, token });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  });
+  .post('/signup', signupUser);
